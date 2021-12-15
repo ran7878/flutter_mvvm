35a389997917de513246ed5afe098f5d2b87c6f5
ran7878 <caoguangdong78@gmail.com>
2021-12-15 15:37:12 +0800

    add: localCache

--- lib/mvvm_provider/view_model/base_view_model.dart
+++ lib/mvvm_provider/view_model/base_view_model.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/widgets.dart';
 
-///ViewModel 中间层 ， 连接数据层和UI层
-abstract class BaseViewModel extends ChangeNotifier{
-  final BuildContext context;
+import '../provider_config.dart';
 
+///ViewModel 中间层 ， 连接数据层和UI层
+abstract class BaseViewModel<T> extends ChangeNotifier{
+  final BuildContext context;
+  T get service => _getService();
   bool disposed = false;
 
   BaseViewModel(this.context);
+
+  T _getService(){
+    return getProvider<T>(context);
+  }
 
   @override
   void dispose() {
--- lib/mvvm_provider/view_model/user_view_model.dart
+++ lib/mvvm_provider/view_model/user_view_model.dart
@@ -9,7 +9,7 @@ import 'package:value_notifier_demo/mvvm_provider/utils/log_util.dart';
 import 'package:value_notifier_demo/mvvm_provider/utils/toast.dart';
 import 'package:value_notifier_demo/mvvm_provider/view_model/base_view_model.dart';
 
-class UserViewModel extends BaseViewModel{
+class UserViewModel extends BaseViewModel<UserService>{
 //1、持有context ， 主动获取Service，用来进行数据层访问
 //2、获取数据后通知UI做出改变notifyListeners
   bool isError = false;
@@ -20,7 +20,7 @@ class UserViewModel extends BaseViewModel{
   UserViewModel(BuildContext context) : super(context);
 
   Future<bool> login(String name, String pwd) async {
-    BaseBean? resp = await getProvider<UserService>(context).login("15501393136", '111111');
+    BaseBean? resp = await service.login("15501393136", '111111');
     if (resp != null) {
       if (resp.code == 200) {
         UserModel userModel = UserModel.fromJson(resp.result);
